438e35bec858ee658fde69fb7981a72bd6ff0866
JacobTapp23 <97190839+JacobTapp23@users.noreply.github.com>
2022-01-10 13:28:14 -0700

    Update curlcommand.bat

--- curlcommand.bat
+++ curlcommand.bat
@@ -1,1 +1,1 @@
-curl -d '{"userName":"WHO@WHO.com", "password":"Kikagabe12$$"}' -H "Content-Type: application/json" -X POST https://dev.stedi.me/login
+curl -d "@data.json" -X POST "https://dev.stedi.me/login"
