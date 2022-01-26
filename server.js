26a67851ea2ee0eaf69b2f1731da46ff235ac785
JacobTapp23 <Jacobtapp12@byui.edu>
2022-01-26 13:45:42 -0700

    API stuff

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const port = 3000;
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello browser');
 });
 
 app.post('/login', (req,res) =>{
-    if(req.body.userName == "jacob"){
+    console.log(JSON.stringify(req.body));
+    if(req.body.userName == "A" && req.body.password == "B"){
         res.send("Welcome!")
     } else{
         res.send("who are you?");
